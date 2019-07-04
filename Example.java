8c22833477403da9a1c58cc907af3d3cb05d7266
omarathon <pixzlert@gmail.com>
2019-07-04 12:24:45 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -6,6 +6,7 @@ import lib.firebasepostjson.lib.FirebaseConnection;
 import net.thegreshams.firebase4j.error.FirebaseException;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Timer;
 
 public class Example {
     public static void main(String[] args) {
@@ -24,16 +25,35 @@ public class Example {
            and would like to store the logs from the listener in C:/data/logs. */
         JsonListener listener;
         try {
-            listener = new JsonListener(connection, Paths.get("C:/data"), "listener", Paths.get("C:/data/logs"));
+            listener = new JsonListener(connection, Paths.get("C:/data/crawler"), "crawler", Paths.get("C:/data/crawler/logs"));
         }
         catch (IOException e) {
             // Failed to construct Listener object.
-            System.err.println("Failure configuring listener!" + e.toString());
+            System.err.println("Failure configuring listener!");
             return;
         }
         // Now open a new thread, and call runListener from that thread to run the listener indefinitely asynchronously.
         new Thread(() -> {
             listener.runListener();
         }).start();
+
+        // -----------------------
+
+        /* For further example, one may wish to schedule a stop() call on the JsonLister object,
+           in some milliseconds amount of time. One achieves this as follows. */
+
+        new Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        // After 60,000 milliseconds (60 seconds = 1 minute), call stop() on the listener.
+                        listener.stop();
+                    }
+                },
+                60000
+        );
+
+        /* What has been achieved is the listener is running on its own thread,
+           and on another thread a timer schedules a stop() call which shall stop the listener after 1 minute. */
     }
 }
